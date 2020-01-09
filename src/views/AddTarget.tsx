3cda68d4122832b4a5df9b4adff92706485d9d44
Nick Welp <niwelp@socialsolutions.com>
2020-01-09 14:01:05 -0600

    added impossible armor saves to test some cases where units gain +2 from cover

--- src/views/AddTarget.tsx
+++ src/views/AddTarget.tsx
@@ -99,6 +99,8 @@ const AddTarget = () => {
                         <option value="4">4+</option>
                         <option value="3">3+</option>
                         <option value="2">2+</option>
+                        <option value="1">1+</option>
+                        <option value="0">0+</option>
                     </select></label>
                     <label>Toughness<select name={'toughness'}>
                         {options(10, 4)}
